a27865c38fc681653c3f1046d4f6e1fae6de29a5
Grace Miller <miller.gra@northeastern.edu>
2024-04-01 10:35:54 -0400

    lab completed

---
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -0,0 +1,220 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function WorkingWithArrays() {
+  const API = "http://localhost:4000/a5/todos";
+  const [todo, setTodo] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-09-09",
+    completed: false,
+  });
+
+  const [todos, setTodos] = useState<any[]>([]);
+  const fetchTodos = async () => {
+    const response = await axios.get(API);
+    setTodos(response.data);
+  };
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  // const removeTodo = async (todo: any) => {
+  //   const response = await axios.get(`${API}/${todo.id}/delete`);
+  //   setTodos(response.data);
+  // };
+
+  const createTodo = async () => {
+    const response = await axios.get(`${API}/create`);
+    setTodos(response.data);
+  };
+
+  const fetchTodoById = async (id: any) => {
+    const response = await axios.get(`${API}/${id}`);
+    setTodo(response.data);
+  };
+
+  const updateTitle = async () => {
+    const response = await axios.get(`${API}/${todo.id}/title/${todo.title}`);
+    setTodos(response.data);
+  };
+
+  const postTodo = async () => {
+    const response = await axios.post(API, todo);
+    setTodos([...todos, response.data]);
+  };
+
+  const deleteTodo = async (todo: any) => {
+    /*const response = */ await axios.delete(`${API}/${todo.id}`);
+    setTodos(todos.filter((t) => t.id !== todo.id));
+  };
+
+  const updateTodo = async () => {
+    /*const response = */ await axios.put(`${API}/${todo.id}`, todo);
+    setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+  };
+
+  return (
+    <div>
+      <h3>Working with Arrays</h3>
+      <label className="form-control">
+        Todo Index
+        <input
+          value={todo.id}
+          onChange={(e) =>
+            setTodo({ ...todo, id: parseInt(e.target.value) || 0 })
+          }
+        />
+      </label>
+      <label className="form-control">
+        Title
+        <input
+          className="form-control"
+          type="text"
+          value={todo.title}
+          onChange={(e) =>
+            setTodo({
+              ...todo,
+              title: e.target.value,
+            })
+          }
+        />
+      </label>
+      <label className="form-control">
+        Completed
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => setTodo({ ...todo, completed: !todo.completed })}
+        />
+      </label>
+      <label className="form-control">
+        Description
+        <input
+          className="form-control"
+          type="text"
+          value={todo.description}
+          onChange={(e) =>
+            setTodo({
+              ...todo,
+              description: e.target.value,
+            })
+          }
+        />
+      </label>
+      <h3>Updating an Item in an Array</h3>
+      <a
+        className="btn btn-primary"
+        href={`${API}/${todo.id}/title/${todo.title}`}
+      >
+        Update Title to {todo.title}
+      </a>
+      <a
+        className="btn btn-primary"
+        href={`${API}/${todo.id}/completed/${todo.completed}`}
+      >
+        Update Completed to {todo.completed.toString()}
+      </a>
+      <a
+        className="btn btn-primary"
+        href={`${API}/${todo.id}/description/${todo.description}`}
+      >
+        Update Description to {todo.description}
+      </a>
+      <h4>Retrieving Arrays</h4>
+      <a className="btn btn-primary" href={API}>
+        Get Todos
+      </a>
+      <h4>Retrieving an Item from an Array by ID</h4>
+      <a className="btn btn-primary" href={`${API}/${todo.id}`}>
+        Get Todo by ID
+      </a>
+      <h3>Filtering Array Items</h3>
+      <a className="btn btn-primary" href={`${API}?completed=true`}>
+        Get Completed Todos
+      </a>
+      <h3>Creating new Items in an Array</h3>
+      <a className="btn btn-primary" href={`${API}/create`}>
+        Create Todo
+      </a>
+      <h3>Deleting from an Array</h3>
+      <a className="btn btn-primary" href={`${API}/${todo.id}/delete`}>
+        Delete Todo with ID = {todo.id}
+      </a>
+      <br />
+      <label>
+        Description
+        <textarea
+          value={todo.description}
+          //type="text"
+          onChange={(e) => setTodo({ ...todo, description: e.target.value })}
+        />
+      </label>{" "}
+      <br />
+      <label>
+        Date
+        <input
+          value={todo.due}
+          type="date"
+          onChange={(e) =>
+            setTodo({
+              ...todo,
+              due: e.target.value,
+            })
+          }
+        />
+      </label>{" "}
+      <br />
+      <label className="form-control">
+        Completed
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) =>
+            setTodo({
+              ...todo,
+              completed: e.target.checked,
+            })
+          }
+        />
+      </label>
+      <button className="btn btn-warning" onClick={postTodo}>
+        {" "}
+        Post Todo{" "}
+      </button>
+      <button className="btn btn-primary" onClick={createTodo}>
+        Create Todo
+      </button>
+      <button className="btn btn-success" onClick={updateTitle}>
+        Update Title
+      </button>
+      <button className="btn btn-secondary" onClick={updateTodo}>
+        Update Todo
+      </button>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            <input checked={todo.completed} type="checkbox" readOnly />
+            <p>{todo.description}</p>
+            <p>{todo.due}</p>
+            {todo.title}
+            <button
+              onClick={() => deleteTodo(todo)}
+              className="btn btn-danger float-end ms-2"
+            >
+              Delete
+            </button>
+            <button
+              className="btn btn-warning float-end ms-2"
+              onClick={() => fetchTodoById(todo.id)}
+            >
+              Edit
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default WorkingWithArrays;
---
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
+
+  const fetchAssignment = async () => {
+    const response = await axios.get(`${ASSIGNMENT_URL}`);
+    setAssignment(response.data);
+  };
+  const updateTitle = async () => {
+    const response = await axios.get(
+      `${ASSIGNMENT_URL}/title/${assignment.title}`
+    );
+    setAssignment(response.data);
+  };
+  useEffect(() => {
+    fetchAssignment();
+  }, []);
+
+  const [module, setModule] = useState({
+    _id: "MATH1234",
+    name: "Week 0 - INTRO",
+    course: "MATH1234",
+    description: "First Week Introduction",
+  });
+  const MODULE_URL = "http://localhost:4000/a5/module";
+
+  return (
+    <div>
+      <h3>Working With Objects</h3>
+
+      <h3>Modifying Properties</h3>
+      <input
+        onChange={(e) =>
+          setAssignment({
+            ...assignment,
+            title: e.target.value,
+          })
+        }
+        value={assignment.title}
+        type="text"
+      />
+      <button className="btn btn-primary" onClick={updateTitle}>
+        Update Title to: {assignment.title}
+      </button>
+      <button className="btn btn-success" onClick={fetchAssignment}>
+        Fetch Assignment
+      </button>
+
+      <h4>Modifying Module Properties</h4>
+      <input
+        className="form-control"
+        type="text"
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+        value={module.name}
+      />
+      <a className="btn btn-success" href={`${MODULE_URL}/name/${module.name}`}>
+        Update Name
+      </a>
+      <h4>Retrieving Objects</h4>
+      <a className="btn btn-primary" href={MODULE_URL}>
+        Get Module
+      </a>
+      <h4>Modifying Assignment Properties</h4>
+      <input
+        className="form-control"
+        type="text"
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+        value={assignment.title}
+      />
+      <a
+        className="btn btn-success"
+        href={`${ASSIGNMENT_URL}/title/${assignment.title}`}
+      >
+        Update Title
+      </a>
+      <label className="form-control">
+        Completed
+        <input
+          type="checkbox"
+          checked={assignment.completed}
+          onChange={() =>
+            setAssignment({ ...assignment, completed: !assignment.completed })
+          }
+        />
+      </label>
+      <a
+        className="btn btn-success"
+        href={`${ASSIGNMENT_URL}/completed/${assignment.completed}`}
+      >
+        Update Completed
+      </a>
+      <label className="form-control">
+        Score
+        <input
+          type="number"
+          onChange={(e) =>
+            setAssignment({ ...assignment, score: parseInt(e.target.value) })
+          }
+        />
+      </label>
+      <a
+        className="btn btn-success"
+        href={`${ASSIGNMENT_URL}/score/${assignment.score}`}
+      >
+        Update Score
+      </a>
+      <h4>Retrieving Objects</h4>
+      <a className="btn btn-primary" href="http://localhost:4000/a5/assignment">
+        Get Assignment
+      </a>
+      <h4>Retrieving Properties</h4>
+      <a
+        className="btn btn-primary"
+        href="http://localhost:4000/a5/assignment/title"
+      >
+        Get Title
+      </a>
+      <a
+        className="btn btn-primary"
+        href="http://localhost:4000/a5/assignment/completed"
+      >
+        Get Completed
+      </a>
+      <a
+        className="btn btn-primary"
+        href="http://localhost:4000/a5/assignment/score"
+      >
+        Get Score
+      </a>
+    </div>
+  );
+}
+export default WorkingWithObjects;
